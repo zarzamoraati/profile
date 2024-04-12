d5697111a5ee0a0b2606002b48cc713e0075e024
zarzamoraati <zarzamoraati@gmail.com>
2024-04-11 22:41:39 -0600

    adding link components

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,10 +1,10 @@
 import { Outlet } from "react-router-dom"
-
+import { Link } from "react-router-dom"
 export const Layout = () => {
   return (
     <div>
-        <a href="/">Home</a>
-        <a href="/about">About</a>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
         <div>
             <Outlet/>
         </div>
