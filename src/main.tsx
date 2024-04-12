048fff811b1737e1a6310c4c3116dc1f5dc12617
zarzamoraati <zarzamoraati@gmail.com>
2024-04-11 22:21:23 -0600

    removing doom

--- src/main.tsx
+++ src/main.tsx
@@ -1,28 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { createBrowserRouter,RouterProvider } from 'react-router-dom'
-import { Layout } from './Layout'
-import { Home } from './components/Home'
-import { About } from './components/About'
+import { App } from './App'
 
-const router=createBrowserRouter([
-  {
-    path:"/",
-    element:<Layout/>,
-    children:[
-      {
-        index:true,
-        element:<Home/>
-      },
-      {
-        path:"/about",
-        element:<About/>
-      }
-    ]
-  }
-])
+// const router=createBrowserRouter([
+//   {
+//     path:"/",
+//     element:<Layout/>,
+//     children:[
+//       {
+//         index:true,
+//         element:<Home/>
+//       },
+//       {
+//         path:"/about",
+//         element:<About/>
+//       }
+//     ]
+//   }
+// ])
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <App/>
   </React.StrictMode>,
 )
